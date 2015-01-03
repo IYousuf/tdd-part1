23891e1188ac5db82dab2625f2f73a2958cd6b9c
IYousuf <imad_yousuf@yahoo.com>
2015-01-03 21:08:01 +0500

    Update MultiCurrenciesTests.java

--- MultiCurrenciesTests.java
+++ MultiCurrenciesTests.java
@@ -1,4 +1,4 @@
-package Chap1;
+package chap11;
 
 import static org.junit.Assert.*;
 
@@ -14,19 +14,16 @@ public class MultiCurrenciesTests {
 	}
 	
 	@Test
-	public void testFrancMultiplication() {
-		Money five = Money.franc(5);
-		assertEquals(Money.franc(10), five.times(2));
-		assertEquals(Money.franc(15), five.times(3));
-	}
-	
-	@Test
 	public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(Money.franc(5).equals(Money.franc(5)));
-		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
-
+	
+	@Test
+	public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
+	}
+	
 }
