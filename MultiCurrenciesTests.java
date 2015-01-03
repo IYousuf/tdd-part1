5d61bb48864a88e0df49a33996fb36ef5ff3a0cd
IYousuf <imad_yousuf@yahoo.com>
2015-01-03 11:06:34 +0500

    Update MultiCurrenciesTests.java

--- MultiCurrenciesTests.java
+++ MultiCurrenciesTests.java
@@ -19,5 +19,13 @@ public class MultiCurrenciesTests {
 		assertEquals(new Franc(10), five.times(2));
 		assertEquals(new Franc(15), five.times(3));
 	}
+	
+	@Test
+	public void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
+		assertTrue(new Franc(5).equals(new Franc(5)));
+		assertFalse(new Franc(5).equals(new Franc(6)));
+	}
 
 }
