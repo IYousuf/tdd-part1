24c6d2aecb5240c0992412e54dcea539f14f7af5
IYousuf <imad_yousuf@yahoo.com>
2015-01-03 10:34:12 +0500

    Create MultiCurrenciesTests.java

---
+++ MultiCurrenciesTests.java
@@ -0,0 +1,23 @@
+package Chap1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MultiCurrenciesTests {
+
+	@Test
+	public void testMultiplication() {
+		Dollar five= new Dollar(5);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
+	}
+	
+	@Test
+	public void testFrancMultiplication() {
+		Franc five= new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
+	}
+
+}
