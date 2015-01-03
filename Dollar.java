e63e5ef91677176dfcc4eaeb32811b315fe6b035
IYousuf <imad_yousuf@yahoo.com>
2015-01-03 17:00:03 +0500

    Update Dollar.java

--- Dollar.java
+++ Dollar.java
@@ -2,16 +2,10 @@ package Chap1;
 
 public class Dollar extends Money{
 	
-	private String currency;
-	
 	Dollar(int amount,String currency){
 		super(amount, currency);
 	}
-	
-	String currency() {
-		return currency;
-	}
-	
+
 	Money times(int multiplier){
 		return Money.dollar(amount * multiplier);
 	}
