61279daa91dacf2f9310dfb314a69bb0ff46655d
IYousuf <imad_yousuf@yahoo.com>
2015-01-03 11:04:57 +0500

    Update Dollar.java

--- Dollar.java
+++ Dollar.java
@@ -1,8 +1,7 @@
 package Chap1;
 
-public class Dollar {
+public class Dollar extends Money{
 	
-	private int amount;
 	Dollar(int amount){
 		this.amount = amount;
 	}
@@ -11,8 +10,4 @@ public class Dollar {
 		return new Dollar(amount * multiplier);
 	}
 	
-	public boolean equals(Object object) {
-		Dollar dollar= (Dollar) object;
-		return amount == dollar.amount;
-	}
 }
