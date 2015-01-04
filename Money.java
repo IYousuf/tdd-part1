7dbe8d4b362f85fed66c39778dabc11c9b5ccf8a
IYousuf <imad_yousuf@yahoo.com>
2015-01-04 09:07:51 +0500

    Update Money.java

--- Money.java
+++ Money.java
@@ -28,7 +28,7 @@ class Money implements Expression{
 		return new Money(amount,"CHF");
 	}
 	
-	Money times(int multiplier) {
+	Expression times(int multiplier) {
 		return new Money(amount * multiplier, currency);
 	}
 	
@@ -36,7 +36,7 @@ class Money implements Expression{
 		return amount + " " + currency;
 	}
 	
-	Expression plus(Money addend) {
+	Expression plus(Expression addend) {
 		return new Sum(this, addend);
 	}
 	
